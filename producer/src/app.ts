6e11bd1a29dd2ffd1b92f3415ec9e67e8e5c4e18
Matheus Flores <Maths.Aki@gmail.com>
2024-11-30 20:50:05 -0300

    mode

--- producer/src/app.ts
+++ producer/src/app.ts
@@ -1,5 +1,6 @@
 // import express from 'express';
 const express = require('express');
+const { Request, Response } = require('express');
 import { config } from 'dotenv';
 import path from 'path';
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Servidor Express rodando!');
 });
 
