6455242a926112aa38e673f08112f0aa8bedd2f2
Matheus Flores <Maths.Aki@gmail.com>
2024-11-30 17:58:18 -0300

    clear

---
+++ Shared/src/initMongo.ts
@@ -0,0 +1,13 @@
+import { connectMongo } from "./mongoClient";
+import { Message } from "./models/message";
+
+const run = async () => {
+  await connectMongo();
+
+  const message = new Message({ content: "Hello from Mongoose!" });
+  await message.save();
+
+  console.log("Message saved:", message);
+};
+
+run();
---
+++ Shared/src/mongoClient.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URL || "mongodb://root:root@localhost:27017/stockwatch?authSource=admin";
+
+export const connectMongo = async () => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB connected!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+};
