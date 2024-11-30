ffbf6f6fb97abd6b05c5a9f26339a03b188b7716
Matheus Flores <Maths.Aki@gmail.com>
2024-11-30 20:47:15 -0300

    require

---
+++ producer/src/app.ts
@@ -0,0 +1,20 @@
+// import express from 'express';
+const express = require('express');
+import { config } from 'dotenv';
+import path from 'path';
+
+config({ path: path.resolve(__dirname, '../../.env') });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor Express rodando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
