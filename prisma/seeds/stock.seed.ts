712a34892ce174ca956a6ebb57112e25b73d0a60
Matheus Flores <Maths.Aki@gmail.com>
2024-12-02 12:36:29 -0300

    text

--- prisma/seeds/stock.seed.ts
+++ prisma/seeds/stock.seed.ts
@@ -34,15 +34,17 @@ async function main() {
     };
 
     // Model no Prisma para servir de exemplo
-    // model Stock {
-    //     id          Int      @id @default(autoincrement())
-    //     productId   Int
-    //     quantity    Int      // Quantidade disponível para essa variação específica
-    //     product     Product  @relation(fields: [productId], references: [id], onDelete: Cascade)
-    //     createdAt   DateTime @default(now())
-    //     updatedAt   DateTime @updatedAt
-    //     StockDetail StockDetail[]
-    //   }
+    /*
+    model Stock {
+        id          Int      @id @default(autoincrement())
+        productId   Int
+        quantity    Int      // Quantidade disponível para essa variação específica
+        product     Product  @relation(fields: [productId], references: [id], onDelete: Cascade)
+        createdAt   DateTime @default(now())
+        updatedAt   DateTime @updatedAt
+        StockDetail StockDetail[]
+    }
+    */
     for (let j = 0; j < variables; j++) {
       const stock = await prisma.stock.create({
         data: {
@@ -53,21 +55,24 @@ async function main() {
       newStock.push(stock.id);
     }
 
-    // model StockDetail {
-    //     id          Int      @id @default(autoincrement())
-    //     stockId     Int
-    //     detailId    Int
-    //     stock       Stock    @relation(fields: [stockId], references: [id], onDelete: Cascade)
-    //     detail      Detail   @relation(fields: [detailId], references: [id], onDelete: Cascade)
-    //     createdAt   DateTime @default(now())
-    //     updatedAt   DateTime @updatedAt
-    //   }
+    // Model no prisma para a relação de stock com detalhes
+    /*
+    model StockDetail {
+        id          Int      @id @default(autoincrement())
+        stockId     Int
+        detailId    Int
+        stock       Stock    @relation(fields: [stockId], references: [id], onDelete: Cascade)
+        detail      Detail   @relation(fields: [detailId], references: [id], onDelete: Cascade)
+        createdAt   DateTime @default(now())
+        updatedAt   DateTime @updatedAt
+    }
+    */
+    const color = variables / divisions[0];
+    const size = variables / divisions[1];
+    const y = variables / divisions[2];
+    const material = variables / divisions[3];
+    const style = variables / divisions[4];
     for (let k = 0; k < newStock.length; k++) {
-      const color = variables / divisions[0];
-      const size = variables / divisions[1];
-      const y = variables / divisions[2];
-      const material = variables / divisions[3];
-      const style = variables / divisions[4];
       await prisma.stockDetail.create({
         data: {}
       })
