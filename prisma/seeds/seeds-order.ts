97f4cf1e70bf77ad56daf0fd5f2d6c7c75d9316a
Matheus Flores <Maths.Aki@gmail.com>
2024-12-03 00:52:29 -0300

    clearCode

--- prisma/seeds/seeds-order.ts
+++ prisma/seeds/seeds-order.ts
@@ -1,12 +1,8 @@
-// 1- details.seed.ts
-// 2- products.seed.ts
-// 3- stock.seed.ts
 import mainD from "./details.seed";
 import mainP from "./products.seed";
 import mainS from "./stock.seed";
 
 async function main() {
-  //
   mainD();
   mainP();
   mainS();
