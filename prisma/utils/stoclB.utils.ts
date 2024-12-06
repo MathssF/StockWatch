d438f41eabb7d9297242d692a3dbc0b2c19611e8
Matheus Flores <Maths.Aki@gmail.com>
2024-12-06 20:47:32 -0300

    new ArrayC

--- prisma/utils/stoclB.utils.ts
+++ prisma/utils/stoclB.utils.ts
@@ -1,18 +1,24 @@
-// type TypeArray = number[]; // Representa qualquer um dos arrays de dados como color, size, year, etc.
-// type Types = boolean[];
+type TypeArray = number[]; // Representa qualquer um dos arrays de dados como color, size, year, etc.
+type Types = boolean[];
 
 export default function generateArrayFinal(
-    stockMatrix: { id: number; matrix: number[] }[],
-    color: number[], size: number[], year: number[], materials: number[], styles: number[],
-    types: boolean[],
-  ) {
-
+  stockMatrix: { id: number; matrix: number[] }[],
+  color: number[], size: number[], year: number[], materials: number[], styles: number[],
+  types: Types
+) {
   const arraysLists = [color, size, year, materials, styles];
-  const finalStock = stockMatrix.map((item) => {
+  const finalStock: { id: number; matrix: number[] }[] = [];
+
+  stockMatrix.forEach((item) => {
     const { id, matrix } = item;
-    
+
     const newMatrix: number[] = [];
-    let index = 0;
+    let arrayIndex = 0;
+
+    // Filtra os arrays de acordo com o tipo ativo em 'types'
+    const activeArrays = types
+      .map((isActive, index) => (isActive ? arraysLists[index] : []))
+      .filter((array) => array.length > 0); // Remove arrays vazios
 
     const generateCombinations = (
       combinations: number[][],
@@ -20,46 +26,19 @@ export default function generateArrayFinal(
       depth: number = 0
     ): void => {
       if (depth === combinations.length) {
-        finalStock.push({ id: id++, matrix: current });
+        finalStock.push({ id, matrix: current }); // Usa o id original de stockMatrix
         return;
       }
-  
+
       for (let item of combinations[depth]) {
         generateCombinations(combinations, [...current, item], depth + 1);
       }
     };
 
-    // matrix.forEach((value) => {
-    //   for (index = 0; index < types.length; index++) {
-    //     if (types[index] === true) {
-    //       switch (index) {
-    //         case 0:
-    //           newMatrix.push(color[value - 1]);
-    //           break;
-    //         case 1:
-    //           newMatrix.push(size[value - 1]);
-    //           break;
-    //         case 2:
-    //           newMatrix.push(year[value - 1]);
-    //           break;
-    //         case 3:
-    //           newMatrix.push(materials[value - 1]);
-    //           break;
-    //         case 4:
-    //           newMatrix.push(styles[value - 1]);
-    //           break;
-    //         default:
-    //           break;
-    //       }
-    //       break;
-    //     }
-    //   }
-    // });
+    // Cria as combinações com os arrays filtrados
+    generateCombinations(activeArrays);
 
-    return {
-      id,
-      matrix: newMatrix
-    };
+    return;
   });
 
   return finalStock;
