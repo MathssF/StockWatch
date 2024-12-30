a874824a5bea14c1951d825422fdf4858cc5eb90
Matheus Flores <Maths.Aki@gmail.com>
2024-12-30 15:17:53 -0300

    newPort

--- Consumer/src/index.ts
+++ Consumer/src/index.ts
@@ -1,5 +1,5 @@
 import app from './app';
-const PORT = Number(process.env.CONSUMER_PORT) || 3013;
+const PORT = Number(process.env.CONSUMER_PORT) || 3023;
 
 app.listen(PORT, 'localhost', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
