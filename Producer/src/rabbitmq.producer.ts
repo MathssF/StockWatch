a8f94ebf1835b1526b879bb896833f195aee3796
Matheus Flores <Maths.Aki@gmail.com>
2025-01-06 14:50:06 -0300

    Resolvido o erro da conexão com o RabbitMQ

--- Producer/src/rabbitmq.producer.ts
+++ Producer/src/rabbitmq.producer.ts
@@ -3,10 +3,11 @@ import { PrismaClient } from '@prisma/client';
 import { v4 as uuidv4 } from 'uuid';
 
 const prisma = new PrismaClient();
-const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://user:password@rabbitmq:5672';
+const RABBITMQ_URL = process.env.RABBITMQ_LOCAL || 'amqp://user:password@rabbitmq:5672';
 
 export async function sendToQueue(queueName: string, message: string, id?: string) {
   try {
+    console.log('Entrou no 1');
     const msgid = uuidv4();
     await prisma.rabbitMQMessage.create({
       data: {
@@ -19,12 +20,19 @@ export async function sendToQueue(queueName: string, message: string, id?: strin
       },
     });
 
+    console.log('Entrou no 2');
+
     const connection = await amqp.connect(RABBITMQ_URL);
     const channel = await connection.createChannel();
+
+    console.log('Entrou no 3');
+
 
     await channel.assertQueue(queueName, {
       durable: false,
     });
+
+    console.log('Entrou no 4');
 
     const messageToSend = id ? JSON.stringify({ id, msgid, message }) : JSON.stringify({ msgid, message});
     channel.sendToQueue(queueName, Buffer.from(messageToSend));
