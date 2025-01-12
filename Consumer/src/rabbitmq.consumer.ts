9506ce62e6322d9620243ec69859d53711622cfa
Matheus Flores <Maths.Aki@gmail.com>
2025-01-12 17:12:50 -0300

    consumer03

--- Consumer/src/rabbitmq.consumer.ts
+++ Consumer/src/rabbitmq.consumer.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 const RABBITMQ_URL = process.env.RABBITMQ_LOCAL || 'amqp://user:password@localhost:5672';
 
-interface MessageData {
+interface QueueMessage {
   id: string;
   msgid: string;
   message: string;
@@ -13,40 +13,39 @@ interface MessageData {
 export async function consumeQueue(
   queueName: string,
   durableValue?: boolean
-): Promise<MessageData[]> {
+): Promise<QueueMessage[]> {
   try {
     console.log('Conectando ao RabbitMQ...');
     const connection = await amqp.connect(RABBITMQ_URL);
-    const channel = await connection.createChannel();
 
+    const channel = await connection.createChannel();
     await channel.assertQueue(queueName, { durable: durableValue ?? false });
+
     console.log(`Aguardando mensagens na fila: ${queueName}`);
 
-    const messages: MessageData[] = [];
+    const messages: QueueMessage[] = [];
 
     await new Promise<void>((resolve) => {
       channel.consume(queueName, (msg) => {
         if (msg) {
-          const messageData: MessageData = {
+          channel.ack(msg);
+
+          messages.push({
             id: msg.properties.correlationId || '',
             msgid: msg.properties.messageId || '',
             message: msg.content.toString(),
-          };
+          });
 
-          messages.push(messageData);
-          channel.ack(msg); // Confirma que a mensagem foi processada.
+          // Resolve imediatamente após consumir a mensagem
+          resolve();
         }
-
-        // Finaliza o consumo após processar uma mensagem
-        resolve();
       });
     });
 
-    console.log('Mensagens consumidas:', messages);
-    return messages; // Retorna todas as mensagens consumidas.
+    return messages;
   } catch (error) {
     console.error('Erro ao consumir fila:', error);
-    return []; // Retorna um array vazio em caso de erro.
+    return []; // Retorna array vazio em caso de erro
   } finally {
     await prisma.$disconnect();
   }
