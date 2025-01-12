2c798888840c3194e84a9be49e05b47add5a3554
Matheus Flores <Maths.Aki@gmail.com>
2025-01-12 17:11:10 -0300

    consumer01

--- Consumer/src/rabbitmq.consumer.ts
+++ Consumer/src/rabbitmq.consumer.ts
@@ -20,13 +20,6 @@ export async function consumeQueue(
     message: string;
   }[]
 > {
-  // const processedMessages: {
-  //   id: string;
-  //   msgid: string;
-  //   message: string;
-  // }[] = [];
-
-  // let durableBoolean = durableValue || false;
 
   try {
     console.log('Conectando ao RabbitMQ...');
@@ -39,71 +32,27 @@ export async function consumeQueue(
 
     await new Promise<void>((resolve) => {
       console.log('Entrou no newPromise');
-      channel.consume(queueName, async (msg: CustomConsumeMessage | null) => {
+      channel.consume(queueName, (msg) => {
         if (msg) {
-          const messageContent = msg.content.toString();
-          console.log(`(CONSUMER) Mensagem recebida: ${messageContent}`);
-
-          // Parse da mensagem recebida
-          let parsedMessage: { id?: string; msgid?: string; message: string };
-          console.log('Parsed Message');
-          try {
-            parsedMessage = JSON.parse(messageContent);
-            console.log('Parsed 2', parsedMessage);
-          } catch (error) {
-            console.error('Erro ao parsear a mensagem. Ignorando...', error);
-            channel.nack(msg, false, false);
-            return;
-          }
-          const { id, msgid, message } = parsedMessage;
-          let dbMessage = await prisma.rabbitMQMessage.findUnique({
-            where: { messageId: msgid },
-          });
-
-          console.log('dbMessage? ', dbMessage);
-
-          if (!dbMessage) {
-            console.log('CONSUMER - Não achou a mensagem no banco de dados, esta criando');
-            // Se não existir, cria um registro
-            dbMessage = await prisma.rabbitMQMessage.create({
-              data: {
-                produceId: id || 'UNKNOWN',
-                consumerId: uuidv4(),
-                messageId: msgid || uuidv4(),
-                queue: queueName,
-                message,
-                status: 'PENDING',
-              },
-            });
-          }
-
-          // console.log('Mensagem processada com sucesso. Atualizando status no banco de dados.');
-
-          // Atualiza o status para "PROCESSED"
-          // await prisma.rabbitMQMessage.update({
-          //   where: { id: dbMessage.id },
-          //   data: { status: 'PROCESSED' },
-          // });
-          // console.log('Status Atualizado');
-
-          // // Adiciona ao array de mensagens processadas
-          // processedMessages.push({
-          //   id: id || 'UNKNOWN',
-          //   msgid: msgid || 'UNKNOWN',
-          //   message,
-          // });
-
           channel.ack(msg);
-        } else {
-          resolve();
+          resolve({
+            id: msg.properties.correlationId,
+            msgid: msg.properties.messageId,
+            message: msg.content.toString(),
+          });
         }
       });
+  
     });
+
+    return message as {
+      id: string;
+      msgid: string;
+      message: string;
+    };
   } catch (error) {
     console.error('Erro ao consumir fila:', error);
   } finally {
     await prisma.$disconnect();
   }
-
-  return processedMessages;
 }
