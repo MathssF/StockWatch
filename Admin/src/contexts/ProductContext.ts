b280c443363a492bcd2e33a2d2e4f7f4c308627d
Matheus Flores <Maths.Aki@gmail.com>
2025-01-17 11:39:21 -0300

    t

---
+++ Admin/src/contexts/ProductContext.ts
@@ -0,0 +1,11 @@
+import React, { createContext, useContext } from "react";
+// import { products } from "./mockData";
+import { }
+
+// export const ProductsContext = createContext(products);
+
+// export const useProducts = () => useContext(ProductsContext);
+
+// export const ProductsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+//   return <ProductsContext.Provider value={products}>{children}</ProductsContext.Provider>;
+// };
