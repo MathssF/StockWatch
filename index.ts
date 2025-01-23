da999672fe98c652f4bdd1a9643fce9f70771874
Matheus Flores <Maths.Aki@gmail.com>
2025-01-23 15:38:08 -0300

    server

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
-import app from './app';
+import express from 'express';
 
-// Importação dos apps de Producer e Consumer
-import producerApp from './Producer/src/app';
-import consumerApp from './Consumer/src/app';
+// Importa as rotas específicas do Producer e Consumer
+import producerStockRoutes from './Producer/src/routes/stock.routes';
+import consumerStockRoutes from './Consumer/src/routes/stock.routes';
 
-// Porta e host usando variáveis de ambiente
+const app = express();
+
+// Middleware para parsing de JSON
+app.use(express.json());
+
+// Adiciona as rotas do Producer e Consumer com os prefixos
+app.use('/producer/stock', producerStockRoutes);
+app.use('/consumer/stock', consumerStockRoutes);
+
+// Inicia o servidor principal
 const PORT = Number(process.env.PORT) || 3000;
 const HOST = process.env.EXPRESS_HOST || '0.0.0.0';
 
-// Configuração das rotas para Producer e Consumer
-app.use('/producer', producerApp); // Rotas do Producer estarão em /producer
-app.use('/consumer', consumerApp); // Rotas do Consumer estarão em /consumer
-
-// Inicia o servidor principal
 app.listen(PORT, HOST, () => {
   console.log(`Servidor Principal rodando em http://${HOST}:${PORT}`);
 });
